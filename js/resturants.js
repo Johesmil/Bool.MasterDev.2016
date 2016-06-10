56239c84353d36a36ce6e3785e05190d0444bc17
David Stenzelius <david.stenzelius@bool.se>
2016-06-10 15:23:48 +0200

    test
    tset

--- js/resturants.js
+++ js/resturants.js
@@ -5,12 +5,12 @@
         var resultString = [""];
         $.each(resturantData, function (key, place){
            if (place.types.join('').indexOf('food') != -1) {
-                resultString.push("<div class='resturant bool-grid-cell'>");
+                resultString.push("<div class='bool-grid-cell'> <div class='bool-content-box'>");
                 if(place.photos !=undefined )  {
                    
                     resultString.push(["<p>",place.photos[0].html_attributions[0],"</p>"].join(''));
                 }
-                resultString.push("</div>");
+                resultString.push("</div></div>");
            }
         });
         debugger
@@ -23,12 +23,12 @@
         var resultString = [""];
         $.each(resturantData, function (key, place){
            if (place.types.join('').indexOf('bar') != -1) {
-                resultString.push("<div class='resturant bool-grid-cell'>");
+                resultString.push("<div class='bool-grid-cell'>");
                 if(place.photos !=undefined)  {
                     resultString.push(["<p>",place.photos[0].html_attributions[0],"</p>"].join(''));
                 }
                 resultString.push(['rating:', place.rating].join(''));
-                resultString.push("</div>");
+                resultString.push("</div></div>");
            } 
         });
         debugger
