e72cbcb64853f1cc59e0e7f4d27a61c7aa150cde
David Stenzelius <david.stenzelius@bool.se>
2016-06-10 15:15:22 +0200

    resturants
    get places

--- js/resturants.js
+++ js/resturants.js
@@ -5,7 +5,12 @@
         var resultString = [""];
         $.each(resturantData, function (key, place){
            if (place.types.join('').indexOf('food') != -1) {
-            resultString.push(["<div class='resturant'>",place.name,"</div>"].join(''));
+                resultString.push("<div class='resturant bool-grid-cell'>");
+                if(place.photos !=undefined )  {
+                   
+                    resultString.push(["<p>",place.photos[0].html_attributions[0],"</p>"].join(''));
+                }
+                resultString.push("</div>");
            }
         });
         debugger
@@ -18,7 +23,12 @@
         var resultString = [""];
         $.each(resturantData, function (key, place){
            if (place.types.join('').indexOf('bar') != -1) {
-                resultString.push(["<div class='resturant'>",place.name,"</div>"].join(''));
+                resultString.push("<div class='resturant bool-grid-cell'>");
+                if(place.photos !=undefined)  {
+                    resultString.push(["<p>",place.photos[0].html_attributions[0],"</p>"].join(''));
+                }
+                resultString.push(['rating:', place.rating].join(''));
+                resultString.push("</div>");
            } 
         });
         debugger
