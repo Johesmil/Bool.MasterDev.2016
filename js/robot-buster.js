ffc61140fcba65a5f57389a9ff1dd5db2c03b7b3
JoelPennegard <joel.pennegard@bool.se>
2016-06-10 15:37:52 +0200

    Robot buster v1

--- js/robot-buster.js
+++ js/robot-buster.js
@@ -4,10 +4,41 @@
 var app = angular.module("RobotBuster", []); 
 app.controller("rbCtrl", function($scope) {
     
-    $scope.contentclass = "nope";
+    $scope.contentclass = "bool-nope";
+    $scope.verified = false;
 
-    $scope.stuffs = ["This", "Is", "Not","Tedious"];
+    $scope.originalStuffs = ["This ", "could ", "have ", "been ", "tedious ", "but ", "it ", "isn't "];
+
+    $scope.stuffs = ["This ", "could ", "have ", "been ", "tedious ", "but ", "it ", "isn't "];
     
+    $scope.inputOrder = [];
+    $scope.inputString = "";
+
+
+    $scope.remove = function(array, index){
+    	$scope.inputString += array[index];
+    	array.splice(index, 1);
+    	var str = "This could have been tedious but it isn't "
+    	if ($scope.inputString.toLowerCase().indexOf(str.toLowerCase()) != -1){
+    		//$scope.contentclass = "bool-yay-no-robot";
+    		$scope.verified = true;
+    	}
+    }
+
+    $scope.retry = function(){
+    	$scope.stuffs = [];
+    	$scope.stuffs = Array.from($scope.originalStuffs);
+    	$scope.inputString = "";
+    	$scope.contentclass = "bool-nope";
+    	$scope.verified = false;
+    }
+
+//http://www.republiquedesmangues.fr/
+
+
+
+
+
     $scope.addStuffs = function () {
         $scope.stuffs.push($scope.addThis);
     } 
@@ -15,8 +46,7 @@ app.controller("rbCtrl", function($scope) {
     	$scope.stuffs.push(CryptoJs.MD5($scope.addThis));
     }
 
-    $scope.remove = function(array, index){
-    	array.splice(index, 1);
-    }
+
+    
 });
 
